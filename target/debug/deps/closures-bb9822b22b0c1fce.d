d875e24e7873b24e43d96b0b291f9999bf1645e2
psatomas <psatomas@gmail.com>
2025-10-18 19:06:58 -0300

    first commit

---
+++ target/debug/deps/closures-bb9822b22b0c1fce.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/closures/target/debug/deps/closures-bb9822b22b0c1fce.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/closures/target/debug/deps/libclosures-bb9822b22b0c1fce.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/closures-e28ddffa68ac72a9.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/closures/target/debug/deps/closures-e28ddffa68ac72a9.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/closures/target/debug/deps/libclosures-e28ddffa68ac72a9.rmeta: src/main.rs
+
+src/main.rs:
