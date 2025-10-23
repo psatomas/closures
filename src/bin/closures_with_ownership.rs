b9835a5d77e5f987a6b88040e26ba430dc9001aa
psatomas <psatomas@gmail.com>
2025-10-23 06:42:34 -0300

    feat: demonstrate closure capture and ownership transfer in Rust

---
+++ src/bin/closures_with_ownership.rs
@@ -0,0 +1,14 @@
+fn main () {
+    let number = 13;
+    let capture_number = || number;
+    let a = capture_number();
+    let b = capture_number();
+    println!("{a} {b} {number}");
+
+    let first_name = String::from("Alice");
+    let capture_string = || {
+        let person = first_name;
+        println!("{person}");
+    };
+    capture_string();
+}
